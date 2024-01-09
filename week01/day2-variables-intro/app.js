193ad8b15593823598af12eb143ac4fff5aaa349
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-01-09 15:58:33 +0000

    add vars demo

---
+++ week01/day2-variables-intro/app.js
@@ -0,0 +1,32 @@
+console.log('Hello, world!')
+
+// Variables 
+
+// let you can reassign
+let myVaraibleName = '341234234'
+
+myVaraibleName = 'sam'
+// const cannot reassign 
+
+const myConstantVaraible = 42
+
+// myConstantVaraible = 'sam2'
+
+// strings are a collection of characters
+let myFirstString = 'this is a string'
+let mySecondString = "This is also a string"
+let myThirdString = `This is also a string`
+
+// numbers are numbers!
+let myNumber = 42
+
+// we can add numbers together
+console.log(42 + 33)
+
+// What happens if we add strings together?
+console.log('hello' + ' world!')
+
+// booleans are true or false. 
+const theTruth = true
+const theLie = false
+
