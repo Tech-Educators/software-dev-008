964efb32b01a6d5433272ad7762f9f657c8c63ff
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-01-11 14:34:36 +0000

    add conditionals demo

---
+++ week01/day4-conditionals/app.js
@@ -0,0 +1,44 @@
+let headerVar = document.getElementById('header')
+
+console.log(headerVar)
+
+let myFavNumber = 42;
+
+if (myFavNumber == 42) {
+    console.log('My fav number is 7!!');
+    sum(10,5)
+} else {
+    console.log(`You didn't guess my number >:(`)
+}
+
+console.log('yooo!')
+
+function sum(a,b) {
+    console.log(a + b)
+}
+
+let weather = 'rainy'
+
+if (weather == 'gloomy') {
+    console.log('Hopefully the sun shines soon')
+} else if (weather == 'rainy') {
+    console.log('get an umbrella')
+} else {
+    console.log('wooo!')
+}
+
+console.log('This is line 30')
+ 
+ 
+
+let myName = prompt('What is your name?')
+console.log(myName)
+// 0.5
+// 0
+let myRandomNumber = Math.floor(Math.random() * 10)
+
+// greater than or equal to
+// if (10 >= 100)
+
+// less than or equal too
+// 10 <= 100
