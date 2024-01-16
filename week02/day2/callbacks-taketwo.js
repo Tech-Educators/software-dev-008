b20cd786f2679f9c8f428af19fa1f5aab0e0ea63
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-01-16 14:50:17 +0000

    add functions take two

--- week02/day2/callbacks-taketwo.js
+++ week02/day2/callbacks-taketwo.js
@@ -11,6 +11,9 @@ greet('Sam', 'js teacher', 'dogs')
 greet('Ross')
 greet('Max')
 
+
+// callback functions are functions we provide to another function as an argument. 
+// we can then invoke that parameter, and it will run whatever function we give to the function as an argument. 
 function runFunction(functionArg) {
     functionArg()
 }
@@ -18,9 +21,13 @@ function runFunction(functionArg) {
 runFunction(function() {console.log(`I'm being run by another function!`)})
 runFunction(callBackFunc)
 
+// this is a function declaration - we're not invoking this function directly, but it's getting invoked by the 'runFunction' as we pass it as an arugment to that function (runFunction). 
 function callBackFunc() {
     console.log(`I'm also being run by another function!`)
 }
+
+
+
 
 
 let objFunc = {
