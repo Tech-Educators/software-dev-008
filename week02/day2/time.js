6c1ec36583d802c5daf065c9576424e18a985612
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-01-16 15:13:47 +0000

    add setinterval and setTimeout

---
+++ week02/day2/time.js
@@ -0,0 +1,25 @@
+console.log('Its that time!')
+
+// setTimeout 
+
+// setTimeout is a function that takes two parameter - a callback function - and a number. 
+
+function changeBG() {
+
+    setTimeout(function() {
+        // stuff I want to do after a set amount of time. 
+        document.body.style.backgroundColor = 'tomato'
+    }, 5000)
+    // 5k miliseconds is 5 seconds. 
+}
+
+const btn = document.getElementById('btn')
+btn.addEventListener('click', changeBG)
+
+const plusP = document.getElementById('plusP')
+
+function addPlus() {
+    plusP.textContent = `${plusP.textContent} +`
+}
+
+setInterval(addPlus, 1000)
