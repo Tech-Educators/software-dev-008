d08af20c2e0eb76343ba1cd58f478fbb79d65fe4
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-01-22 15:25:43 +0000

    aria demo

--- week03/api-demo/app.js
+++ week03/api-demo/app.js
@@ -1,6 +1,13 @@
 const form = document.getElementById('form')
 const imagecontainer = document.getElementById('img-container')
 
+const announcer = document.getElementById('announcer')
+
+let randomButton = document.getElementById('randomize-button')
+
+let images = []
+let currentImgIndex = 0
+
 form.addEventListener('submit', function(event) {
     event.preventDefault()
     console.log(event)
@@ -11,13 +18,29 @@ form.addEventListener('submit', function(event) {
 
 async function search(queryParam) {
     let response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${queryParam}&client_id=LMIpfPZ7y7PH_GClYLw0JwRMTXqiIgSPDC61Xp2sV84`)
-    console.log(response)
     let data = await response.json()
-    console.log(data.results[0].urls.raw)
+    images = data.results
+    createImages(images[currentImgIndex])
+}
 
-    let img = document.createElement('img')
-    img.src = data.results[0].urls.raw
+function createImages(image) {
+    imagecontainer.innerHTML = ''
+    let imgTag = document.createElement('img')
+    imgTag.src = image.urls.raw
+    imgTag.alt = image.alt_description // set our alt tag for screenreaders
 
-    imagecontainer.appendChild(img)
+    imagecontainer.appendChild(imgTag)
+    annouceAltText(image.alt_description)
 
 }
+
+function annouceAltText(altText) {
+    announcer.textContent = `New image displayed: ${altText}`
+}
+
+randomButton.addEventListener('click', function() {
+    if (images.length > 0) {
+        currentImgIndex = Math.floor(Math.random() * images.length);
+        createImages(images[currentImgIndex])
+    }
+})
