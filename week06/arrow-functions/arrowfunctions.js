b6cb36be296028e3e5199f2184d8b8515f632c0a
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-02-12 14:47:32 +0000

    arrow func example

---
+++ week06/arrow-functions/arrowfunctions.js
@@ -0,0 +1,32 @@
+function sayHello() {
+    return 'Hello'
+}
+
+// Hello
+
+
+const sayGoodbye = () => {
+    return 'Goodbye'
+}
+
+// fancy!
+button.addEventListener('click', () => console.log('Hello'))
+// have to have parenthesis for no parameters
+// no parenthesis needed for one parameter
+// parenthesis needed for more than one parameter
+
+// can return one same line without {}'s
+// if you do have {}'s you have to return. 
+
+// function expression
+const addInterrobang = string => string + '?!'
+
+// function declaration 
+function addInterrobangAgain() {
+
+}
+
+addInterrobang('Hello')
+const greet = () => {
+    return 'Hello there!'
+}
