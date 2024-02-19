3b895e5694125143b8f68a83b8bd9d34eb76f27c
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-02-19 14:45:37 +0000

    add react forms

---
+++ week07/react-forms/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+
+export default function App() {
+
+  const [input, setInput] = useState('')
+
+  const [form, setForm] = useState({
+    location: '',
+    destination: '',
+    notes: ''
+  })
+  function handleChange(e) {
+    console.log(e)
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  console.log(form)
+  return (
+    <div>
+      {/* simple input */}
+      {/* <form>
+        <input type="text" onChange={(e) => {
+          setInput(e.target.value)
+        }}/>
+      </form> */}
+
+      {/* More complext input */}
+      <form>
+        <input name='location' type='text' onChange={handleChange} id='01'/>
+        <input name='destination' type='text' onChange={handleChange} id='02'/>
+        <input name='notes' type='text' onChange={handleChange} id='03'/>
+      </form>
+      <p>You inputed: {input} </p>
+      <Counter />
+    </div>
+  )
+}
+
+function Counter() {
+  const [counter, setCounter] = useState(0)
+  return (
+    <button onClick={() => setCounter(counter + 1)}>click me</button>
+  )
+}
